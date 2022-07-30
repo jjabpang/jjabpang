c412c0e0a16e51b2309fbc09751872fb65078311
jihye94 <qkrtkdwns3410@naver.com>
2022-07-31 00:57:41 +0900

    spring boot 초기세팅

---
+++ src/main/java/com/dongnae/jjabpang/JjabpangApplication.java
@@ -0,0 +1,13 @@
+package com.dongnae.jjabpang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjabpangApplication {
+      
+      public static void main(String[] args) {
+            SpringApplication.run(JjabpangApplication.class, args);
+      }
+      
+}
