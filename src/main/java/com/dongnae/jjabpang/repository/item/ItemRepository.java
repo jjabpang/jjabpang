ab2defe2838ea8fc9e8758f97d2208edf9502883
jihye94 <qkrtkdwns3410@naver.com>
2022-08-07 14:49:49 +0900

    1. 주문 기능 추가완료!

--- src/main/java/com/dongnae/jjabpang/repository/item/ItemRepository.java
+++ src/main/java/com/dongnae/jjabpang/repository/item/ItemRepository.java
@@ -1,14 +1,11 @@
 package com.dongnae.jjabpang.repository.item;
 
-import com.dongnae.jjabpang.entity.Category;
 import com.dongnae.jjabpang.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
-      Optional<Item> findItemsByCategoryOrderByCdtDesc(Category category);
+
       
 }
