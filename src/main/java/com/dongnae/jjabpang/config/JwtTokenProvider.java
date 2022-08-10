fd6ea4ddfbc6b3c466acef28ca964f059f29212a
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-08-10 19:14:43 +0900

    feat: 스프링 시큐리티 적용 및 JWT 적용 준비
    Author: @qkrtkdwns3410

---
+++ src/main/java/com/dongnae/jjabpang/config/JwtTokenProvider.java
@@ -0,0 +1,44 @@
+package com.dongnae.jjabpang.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/*
+ *packageName    : com.dongnae.jjabpang.config
+ * fileName       : JwtTokenProvider
+ * author         : ipeac
+ * date           : 2022-08-10
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-08-10        ipeac       최초 생성
+ */
+//해당 클래스는 JwtTokenProvider가 검증을 끝낸 Jwt로부터 유저 정보를 조회해와서 UserPasswordAuthenticationFilter 로 전달합니다.
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends GenericFilterBean {
+      
+      private final JwtTokenProvider jwtTokenProvider;
+      
+      @Override
+      public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            // 헤더에서 JWT 를 받아옵니다.
+            String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+            // 유효한 토큰인지 확인합니다.
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                  // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+                  Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                  // SecurityContext 에 Authentication 객체를 저장합니다.
+                  SecurityContextHolder.getContext()
+                                       .setAuthentication(authentication);
+            }
+            chain.doFilter(request, response);
+      }
