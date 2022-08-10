4b6c031a4315834340ec67b2a7dd38507f40ed7f
qkrtkdwns3410 <qkrtkdwns3410@github.com>
2022-08-11 04:06:04 +0900

    fix: 스프링 시큐리티 csrf 에러 해결
    csrf : 크로스 사이트 요청 위조, 인증된 사용자가 웹 애플리케이션에 특정 요청을 보내도록 유도하는 공격 행위
    
    rest api 의 경우에는 별도 요청에 인증정보를 포함시키기 때문에 불필요한 csrf가 발생한다. 비활성화로 에러해결
    
    Author: @qkrtkdwns3410

--- src/main/java/com/dongnae/jjabpang/config/WebSecurityConfig.java
+++ src/main/java/com/dongnae/jjabpang/config/WebSecurityConfig.java
@@ -36,6 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       
       @Override
       protected void configure(HttpSecurity http) throws Exception {
+            http.csrf()
+                .disable();
             http.httpBasic()
                 .and()
                 .authorizeRequests()
