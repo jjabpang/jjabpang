efb2ee9455c581a6b7ff5b721afae95c65830a69
jihye94 <qkrtkdwns3410@naver.com>
2022-08-15 03:43:11 +0900

    feat : 무중단 배포 구현중
    테스트 리소스 루트로 수정
    
    Author: @qkrtkdwns3410
    Signed-off-by: jihye94 <qkrtkdwns3410@naver.com>

---
+++ src/main/java/com/dongnae/jjabpang/controller/LoggingController.java
@@ -0,0 +1,34 @@
+package com.dongnae.jjabpang.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.dongnae.jjabpang.controller
+ * fileName       : LoggingController
+ * author         : qkrtkdwns3410
+ * date           : 2022-08-15
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2022-08-15        qkrtkdwns3410       최초 생성
+ */
+@RestController
+public class LoggingController {
+      
+      @Value("${logging-module.version}")
+      private String version;
+      
+      @GetMapping("/")
+      public String version() {
+            return String.format("Project Version : %s", version);
+      }
+      
+      @GetMapping("/health")
+      public String checkHealth() {
+            return "healthy";
+      }
+      
+}
